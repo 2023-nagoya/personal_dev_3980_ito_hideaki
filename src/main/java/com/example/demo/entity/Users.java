edd4d8e36def503c10d96a24320fb7aed5d48067
student <student@WIN-OFVMGR8UNES.lan>
2023-05-18 09:41:38 +0900

    保存

---
+++ src/main/java/com/example/demo/entity/Users.java
@@ -0,0 +1,36 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Users {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+	private String password;
+	private String email;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+}
